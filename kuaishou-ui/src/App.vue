62a74bbe3528f6d7e447023a3ee091956fa33c8c
null <null@null-AI-Series.local>
2026-05-02 05:27:21 +0800

    chore: reorder nav and update sid label

--- kuaishou-ui/src/App.vue
+++ kuaishou-ui/src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-app-bar density="compact" color="surface">
       <v-tabs v-model="tab" align-tabs="center">
-        <v-tab to="/account">账户</v-tab>
         <v-tab to="/">首页</v-tab>
+        <v-tab to="/account">账户</v-tab>
         <v-tab to="/mcn">电商MCN</v-tab>
         <v-tab to="/guild">娱乐公会</v-tab>
         <v-tab to="/agency">公会机构</v-tab>
